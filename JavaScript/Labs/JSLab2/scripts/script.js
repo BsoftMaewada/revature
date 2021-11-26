b0c70f4345925bfa83c7438274750e4a93532265
Buhari Nasir Maiwada <maewada@live.com>
2021-11-26 03:52:33 -0500

    first commit

---
+++ JavaScript/Labs/JSLab2/scripts/script.js
@@ -0,0 +1,73 @@
+//Add your work to this file. 
+
+/* Example
+This is a variable declaration. 
+Also referred to as a variable definition.
+It typically will have a keyword and then the name 
+of the variable. In this case, the variable is named 
+firstVariable. The keyword let indicates to JavaScript
+that we are defining a new variable.*/
+let firstVariable; 
+
+/* Your turn 
+Define a variable named data */
+let data; 
+
+/* Example
+ Here we initialize firstVariable
+ In other words we assign it a value for the first
+ time. */
+ firstVariable = 100;
+
+ /* Now we can use our variable. 
+ Let's print it to the console.
+ 
+ Notice that to use our variable we don't need to
+ use the keyword let again. We just use the name of 
+ the variable.*/
+ console.log(firstVariable);
+
+
+ /*Your turn
+ Initialize data to 5000*/
+data = 5000;
+ /*Then print your variable to the console.*/
+
+console.log(data);
+ 
+const consVariable = "Can't be changed"
+console.log(consVariable);
+
+
+/* Your turn
+
+In the next lines of your program, assign your variables, firstVariable and data, new values.  
+firstVariable should hold the name of your favorite food. Hint: In order to store text values you must include the text in quotation marks. 
+data should be assigned your favorite number. 
+
+Print these values to the console. */
+    
+firstVariable = "Taco";
+console.log(firstVariable);
+
+data = 7;
+console.log(data);
+
+/* Together
+Now let's try to assign const a new value. 
+Add this line of code to the end of your program. 
+
+constVariable = "Reassignment"; */
+
+//const consVariable = "Reassignment"
+
+var myVar;
+myVar = true;
+myVar = false;
+console.log(myVar);
+
+console.log(varVariable);
+console.log(abc);
+
+let abc;
+var varVariable;
---
+++ JavaScript/LessonVideos/lessonVideos.js
@@ -0,0 +1,69 @@
+// alert("Hello World ");
+
+// var num;
+// num = 3;
+// var myName;
+// myName = "Buhari"
+
+// var firstname = "Maryam";
+
+// var carSpec = {
+//     numWheels: 4,
+//     engine: "V6",
+//     interior: "cloth"
+// }
+// // Variable Re-assignment
+// var a;
+// a = 25;
+// a = 13;
+// a = "Hello";
+
+// // Copying Values
+
+// var a = 23;
+// var b = a; //b is now 23
+
+// // Re-Assigning Null: to set a variable to an empty value use null.
+
+// var a = 23;
+// a = null;
+
+// var b;
+// console.log(b);
+
+// b = 23;
+// console.log(b);
+
+// b = "Adam";
+// console.log(b);
+
+// b = null;
+
+// var x;
+// console.log(x);
+
+// // Datatypes
+// // Objects represents a group of key/value pairs
+// // Array represents a collection of data
+
+// // Operators
+
+// var y = 2;
+// var z = 6;
+
+// console.log(y + z);
+// console.log(y - z);
+// console.log(y * z);
+// console.log(y / z);
+// console.log(y % z);
+// console.log(y / 0);
+
+var p;
+p = true;
+n = false;
+console.log(p);
+
+var num = 14;
+var s = "Hello";
+s = s + num + 23;
+console.log(s);
